0a6d3e3adc1b8313623fd349996a17ea687b86d4
Nugraha1313 <aulia.nugraha13@gmail.com>
2022-10-05 23:47:21 +0700

    add tugas 6

---
+++ belajar/Personal/navbar.php
@@ -0,0 +1,11 @@
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link active" href="index.php?page=home">Home</a></li>
+          <li><a class="nav-link" href="index.php?page=about">About</a></li>
+          <li><a class="nav-link" href="index.php?page=resume">Resume</a></li>
+          <li><a class="nav-link" href="index.php?page=services">Services</a></li>
+          <li><a class="nav-link" href="#portofolio">Portfolio</a></li>
+          <li><a class="nav-link" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
